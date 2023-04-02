8fe5d23453afb393039a5a5c54f6b9caf8cfe55d
Luis Maia <luismaiasombra.ufc@gmail.com>
2023-04-02 16:52:16 -0300

    Fix boilerplate

--- myApp.js
+++ myApp.js
@@ -1,39 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word,
+  });
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
