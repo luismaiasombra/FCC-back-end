84fc5837976f26081e119d2e72e8fd875a87e9ec
luismaiasombra <95807950+luismaiasombra@users.noreply.github.com>
2023-04-19 18:03:44 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,14 +1,23 @@
-let express = require("express");
-let app = express();
-const { port } = require("./server.js");
+const http = require('http');
+const hostname = '172.31.89.181';
+const port = 3000;
+const express = require("express")
+const app = express();
+/*
+
+
+app.get('/:word/echo', (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
+
+;*/
 app.get("/:word/echo", (req, res) => {
   const { word } = req.params;
   res.json({
-    echo: word,
+    echo: word
   });
 });
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-module.exports = app;
+  console.log(`Server running at ${hostname}:${port}`);
+})
